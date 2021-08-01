e13429c7818cc455ef60a6eee9133611b693d635
zouxlin3 <zouxlin3@qq.com>
2021-08-01 11:28:31 +0800

    new:delete(bug)&complete function

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ListSort from './ListSort.jsx';
+import { Button} from 'antd';
+import { DeleteOutlined, CheckOutlined } from '@ant-design/icons';
 import '../index.css';
 import '../assets/List.css';
 import axios from 'axios'
@@ -36,14 +38,57 @@ class List extends React.Component {
       })
     }
 
+    delete(id){
+      axios
+      .get(api+'delete?id='+id)
+
+      var todos = this.state.todos
+      for (var i=0; i<todos.length; i++){
+        if(todos[i]['id'] == id){
+          todos.splice(i, 1)
+          this.setState({
+            todos: todos
+          })
+          break
+        }
+      }
+    }
+
+    complete(id){
+      axios
+      .get(api+'complete?id='+id)
+
+      var todos = this.state.todos
+      for (var i=0; i<todos.length; i++){
+        if(todos[i]['id'] == id){
+          todos[i]['is_completed'] = !todos[i]['is_completed']
+          this.setState({
+            todos: todos
+          })
+          break
+        }
+      }
+    }
+
+    todo_content(content, is_completed){
+      if(is_completed){
+        return <p><s>{content}</s></p>
+      }
+      else {
+        return <p>{content}</p>
+      }
+    }
+
     render() {
       const Todo = this.state.todos.map((todo, i) => {
-        const { content, is_completed } = todo;
+        const { id, content, is_completed } = todo;
         return (
-          <div key={i} className={`${this.props.className}-card`}>
+          <div key={id+''+i} className={`${this.props.className}-card`}>
             <div className={`${this.props.className}-content`}>
-              <p>{content}</p>
+              {this.todo_content(content, is_completed)}
             </div>
+            <Button type="primary" shape="circle" icon={<DeleteOutlined />} onClick={this.delete.bind(this, id)} />
+            <Button type="primary" shape="circle" icon={<CheckOutlined />} onClick={this.complete.bind(this, id)} />
           </div>
         );
       });
